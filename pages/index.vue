2702acd1c2084626bb829b9c8338e2873d4455fc
MassimoI <massimoi@nkoni.org>
2023-04-04 09:45:18 +0200

    initial import

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>Gestione Posteggi</h2>
+        <table border="2" cellpadding="5" cellspacing="2">
+            <tr v-for="b in 10">
+                <td v-for="a in 10">Posto {{ a*b }}</td>
+            </tr>
+        </table>        
+    </div>
+</template>
+
+<script>
+import Util from "@/mixing/util";
+
+
+export default {
+	name: "Parking",
+	mixins: [Util],
+	data() {
+		return {
+			
+		};
+	},
+	computed: {},
+	methods: {
+		nextTab(next) {
+			this.$router.push(`/bilancio/${next}`);
+		},
+	},
+};
+</script>
+
+<style>
+</style>
---
+++ routes.js
@@ -0,0 +1,36 @@
+import DashboardLayout from "~theme/DashboardLayout.vue";
+
+const routes = [
+    {
+		path: "/",
+		component: DashboardLayout,
+		redirect: "/dashboard",
+		children: [
+			{
+				path: "/parking",
+				name: "Gestione Posteggi",
+				meta: { authorize: ["admin"] },
+				// lazy-loaded
+				component: pageComponent("index"),
+			},						
+		]
+	}
+];
+
+/**
+ * Asynchronously load view (Webpack Lazy loading compatible)
+ * The specified component must be inside the Views folder
+ * @param  {string} name  the filename (basename) of the view to load.
+ * **/
+function pageComponent(name) {	
+	var res;	
+		try {
+			res = require("@/extra/Parking/pages/" + name + ".vue").default;
+		} catch (e) {
+			console.log("Errore route");
+			res = require("./pages/" + name + ".vue").default;
+		}	
+	return res;
+}
+
+export default routes;
